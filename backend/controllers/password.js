fec33c970ce56765167c15fcaadfe242fb6bcc36
sumitpadney12 <heerjnv@gmail.com>
2024-02-20 18:50:24 +0530

    Email Edit

--- backend/controllers/password.js
+++ backend/controllers/password.js
@@ -29,7 +29,7 @@ exports.sendOTP = async (req, res) => {
       sender,
       to: [
         {
-          email: "sumitpandeyjnv@gmail.com",
+          email: email,
           name: "Sumit Pandey",
         },
       ],
